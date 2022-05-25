021d56c694c7d3900e50c2b154a0cd89a9ebe1e2
vader9595 <102444679+vader9595@users.noreply.github.com>
2022-05-24 20:36:14 -0500

    Initial commit

---
+++ src/main/java/com/NicksWorld3/SoloProgram3/SoloProgram3Application.java
@@ -0,0 +1,13 @@
+package com.NicksWorld3.SoloProgram3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProgram3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProgram3Application.class, args);
+	}
+
+}
